c055be9a6ecad48400dfe68507f0ff5d23520c71
Dan <dlangridge@gmail.com>
2015-11-17 14:19:04 -0800

    Added Model View Projection Transformations

--- Glitter/Sources/engine.cpp
+++ Glitter/Sources/engine.cpp
@@ -69,7 +69,17 @@ namespace Gravel
     void Engine::mainLoop() {
 
         auto t_start = std::chrono::high_resolution_clock::now();
-        
+
+        glm::mat4 view = glm::lookAt(
+                glm::vec3(1.2f, 1.2f, 1.2f),
+                glm::vec3(0.0f, 0.0f, 0.0f),
+                glm::vec3(0.0f, 0.0f, 1.0f)
+                );
+        shader->bind("view", view);
+
+        glm::mat4 proj = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 1.0f, 10.0f);
+        shader->bind("proj",proj);
+ 
         while (glfwWindowShouldClose(mWindow) == false) {
             if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
                 glfwSetWindowShouldClose(mWindow, true);
@@ -81,17 +91,17 @@ namespace Gravel
             auto t_now = std::chrono::high_resolution_clock::now();
             float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
             
-            glm::mat4 trans;
-            trans = glm::rotate(
-                    trans,
+            glm::mat4 model;
+            model = glm::rotate(
+                    model,
                     time * glm::radians(180.0f), 
                     glm::vec3(0.0f, 0.0f, 1.0f)
                     );
 
-            glm::vec4 result = trans * glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
+            glm::vec4 result = model * glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
             printf("%f, %f, %f,\n", result.x, result.y, result.z);
             
-            shader->bind("trans", trans);
+            shader->bind("model", model);
              
             glDrawArrays(GL_TRIANGLES, 0, 3);
 
